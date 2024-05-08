019f6e3bcaa599b7cd31d4caa37a946acfdab7cd
voric <voricyt@gmail.com>
2024-05-08 09:19:53 +0200

    Change volatile variables to non-volatile, add debounce handling in loop(), remove interrupt attachments
    - Replaced volatile variables with non-volatile counterparts.
    - Added debounce delay handling in the loop() function using

--- src/main_int.cpp
+++ src/main_int.cpp
@@ -4,11 +4,11 @@
 #define BTN_PIN 13          
 #define BTN_START_PIN 15    
 
-volatile unsigned long startTime;
-volatile unsigned long reactionTime;
-volatile bool testActive = false;
-unsigned long lastDebounceTime = 0;  
-unsigned long debounceDelay = 50;    
+unsigned long startTime;
+unsigned long reactionTime;
+bool testActive = false;
+unsigned long lastButtonPressTime = 0;
+unsigned long debounceDelay = 50;
 
 void startReactionTest() {
   Serial.println("Bereit... Achte auf die LED!");
@@ -19,34 +19,10 @@ void startReactionTest() {
   testActive = true;            
 }
 
-void handleStartButton() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastDebounceTime > debounceDelay) {
-    startReactionTest();
-    lastDebounceTime = currentMillis;
-  }
-}
-
-void handleReactionButton() {
-  unsigned long currentMillis = millis();
-  if (testActive && (currentMillis - lastDebounceTime > debounceDelay)) {
-    digitalWrite(LED_PIN, LOW); 
-    reactionTime = currentMillis - startTime; 
-    Serial.print("Reaktionszeit: ");
-    Serial.print(reactionTime);
-    Serial.println(" ms");
-    testActive = false; 
-    Serial.println("Drücke den Startknopf oder sende 'r', um erneut zu starten.");
-    lastDebounceTime = currentMillis;
-  }
-}
-
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT_PULLUP);
   pinMode(BTN_START_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(BTN_START_PIN), handleStartButton, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BTN_PIN), handleReactionButton, FALLING);
   Serial.begin(115200);
   randomSeed(analogRead(0));
   Serial.println("Drücke den Startknopf oder sende 'r' oder 'd <ms>' im seriellen Monitor, um zu starten bzw. Debounce-Zeit zu setzen.");
@@ -56,15 +32,36 @@ void loop() {
   if (Serial.available() > 0) {
     char c = Serial.read();
     if (c == 'd') {
-      if (Serial.available() > 0) {
-        debounceDelay = Serial.parseInt();
-        Serial.print("Debounce-Zeit gesetzt auf: ");
-        Serial.print(debounceDelay);
-        Serial.println(" ms");
-      }
-    }
-    else if (c == 'r') {
+      debounceDelay = Serial.parseInt();
+      Serial.print("Debounce-Zeit gesetzt auf: ");
+      Serial.print(debounceDelay);
+      Serial.println(" ms");
+    } else if (c == 'r') {
       startReactionTest();
+    }
+  }
+
+  unsigned long currentMillis = millis();
+
+  // Startknopf Debouncing
+  if (digitalRead(BTN_START_PIN) == LOW) {
+    if (currentMillis - lastButtonPressTime > debounceDelay) {
+      lastButtonPressTime = currentMillis;
+      startReactionTest();
+    }
+  }
+
+  // Reaktionsknopf Debouncing
+  if (testActive && digitalRead(BTN_PIN) == LOW) {
+    if (currentMillis - lastButtonPressTime > debounceDelay) {
+      digitalWrite(LED_PIN, LOW);
+      reactionTime = currentMillis - startTime;
+      Serial.print("Reaktionszeit: ");
+      Serial.print(reactionTime);
+      Serial.println(" ms");
+      testActive = false;
+      Serial.println("Drücke den Startknopf oder sende 'r', um erneut zu starten.");
+      lastButtonPressTime = currentMillis;
     }
   }
 }
