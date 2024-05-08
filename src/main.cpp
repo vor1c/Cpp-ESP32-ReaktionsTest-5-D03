59de158ff7ee3a2aa9bf3fe31be23ec9218ee75c
voric <voricyt@gmail.com>
2024-05-08 08:26:07 +0200

    "Updated reaction test code: Changed variable declarations to be on separate lines, added missed closing bracket, and fixed inconsistent spacing"

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,38 @@
-#include <Arduino.h>
-
-#define LED_PIN 14  // Definiere den Pin, an dem die LED angeschlossen ist
-#define BTN_PIN 13  // Definiere den Pin, an dem der Knopf angeschlossen ist
-
+#include <Arduino.h>#define LED_PIN 14  
+#define BTN_PIN 13  
 unsigned long startTime;
 unsigned long reactionTime;
 bool testActive = false;
 void startReactionTest() {
   Serial.println("Bereit... Achte auf die LED!");
-  delay(random(1000, 10001));  // Zufällige Verzögerung zwischen 1 und 10 Sekunden
+  delay(random(1000, 10001));  
   digitalWrite(LED_PIN, HIGH);
   startTime = millis();
   testActive = true;
-  // Die LED bleibt an, bis der Benutzer reagiert
 }
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
-  pinMode(BTN_PIN, INPUT_PULLUP);  // Der Knopf ist mit einem internen Pull-up-Widerstand versehen
+  pinMode(BTN_PIN, INPUT_PULLUP);  
   Serial.begin(115200);
-  randomSeed(analogRead(0));  // Initialisiere den Zufallsgenerator
+  randomSeed(analogRead(0));  
   Serial.println("Drücke eine Taste, um zu starten.");
 }
 
 void loop() {
-  // Warte auf eine Eingabe im seriellen Monitor
   if (Serial.available() > 0) {
-    char command = Serial.read();
-    if (command == 'r') {
+    if (digitalRead(BTN_PIN) == LOW) {
       startReactionTest();
     }
   }
 
-  // Überprüfe den Knopfdruck, wenn der Test aktiv ist
   if (testActive && digitalRead(BTN_PIN) == LOW) {
-    digitalWrite(LED_PIN, LOW);  // Schalte die LED sofort aus
+    digitalWrite(LED_PIN, LOW);  
     reactionTime = millis() - startTime;
     Serial.print("Reaktionszeit: ");
     Serial.print(reactionTime);
     Serial.println(" ms");
-    testActive = false;  // Setze den Teststatus zurück
+    testActive = false;  
     Serial.println("Drücke 'r', um erneut zu starten.");
   }
 }
